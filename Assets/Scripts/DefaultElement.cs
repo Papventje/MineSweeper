7f3f855b2a2c2f69e020600d65ffe2fb3041a7e6
Papventje <robbierobsnor@gmail.com>
2017-09-26 11:02:43 +0200

    Minesweeper Done

---
+++ Assets/Scripts/DefaultElement.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultElement : MonoBehaviour
+{
+    public bool mine;
+
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+    public Sprite flagTexture;
+    public Sprite spriteTexture;
+
+    void Start()
+    {
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        Grid.elements[x, y] = this;
+        // randomise mines
+        mine = UnityEngine.Random.value < 0.15;
+
+    }
+
+    //load textures on click
+    public void LoadTexture(int adjacentCount)
+    {
+        if (mine)
+        {
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        }
+        else
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+    }
+
+
+    public bool Iscovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "default";
+    }
+
+    
+    void OnMouseUpAsButton()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (GetComponent<SpriteRenderer>().sprite == flagTexture)
+            {
+                GetComponent<SpriteRenderer>().sprite = spriteTexture;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().sprite = flagTexture;
+            }
+        }
+        if (!Input.GetKeyDown(KeyCode.LeftShift)){
+            if (mine)
+            {
+                // uncover all mines
+                Grid.uncoverMines();
+                // game over
+                print("you lose");
+            }
+            else
+            {
+                int x = (int)transform.position.x;
+                int y = (int)transform.position.y;
+                LoadTexture(Grid.adjacentMines(x, y));
+
+                //uncover mineless area
+                Grid.FFuncover(x, y, new bool[Grid.w, Grid.h]);
+
+                if (Grid.isFinished())
+                    print("you win");
+
+            }
+        }
+    }
+        
+}
---
+++ Assets/Scripts/RestartWithButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RestartWithButton : MonoBehaviour {
+
+    [SerializeField]
+    private Button restartButton;
+
+    void Start()
+    {
+        Button btn = restartButton.GetComponent<Button>();
+        btn.onClick.AddListener(RestartLevel);
+    }
+
+	void RestartLevel () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
